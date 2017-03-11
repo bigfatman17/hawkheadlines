aa31b0ffe2bbf31d09e723b5b3a76516460007c0
armv7 <bigfatman17@gmail.com>
2017-03-10 20:53:42 -0500

    Successfully parsing the Hawk Headlines RSS feed. Printing the title as a test.

--- app/src/main/java/net/hawkheadlines/hawkheadlines/Main.java
+++ app/src/main/java/net/hawkheadlines/hawkheadlines/Main.java
@@ -1,13 +1,50 @@
 package net.hawkheadlines.hawkheadlines;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main extends AppCompatActivity {
+
+    private final String RSS_FEED_URL = "http://hawkheadlines.net/news/feed/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Download and parse the RSS feed.
+        new GetFeedTask().execute();
+    }
+
+    /* Called once the feed has been downloaded and parsed. */
+    private void onFeedReceived(SyndFeed feed) {
+        Toast.makeText(this, feed.getTitle(), Toast.LENGTH_SHORT).show();
+    }
+
+    /** Asynchronous task that downloads the RSS feed.
+     *  Android highly suggests that we do not run internet connections on the main UI thread.
+     */
+    private class GetFeedTask extends AsyncTask<Void, Void, SyndFeed>
+    {
+        protected SyndFeed doInBackground(Void... par) {
+            SyndFeed feed = null;
+            try { feed = new SyndFeedInput().build(new XmlReader(new URL(RSS_FEED_URL))); }
+            catch(FeedException | IOException e) { Log.e("ERROR", "CANNOT READ RSS FEED", e); }
+            return feed;
+        }
+
+        protected void onPostExecute(SyndFeed res) { onFeedReceived(res); }
     }
 }
